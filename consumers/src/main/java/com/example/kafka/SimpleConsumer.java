b078f9336af4002c999c4142e6868659948b6d29
daeun.kim <daeun.kim@wesang.com>
2023-01-28 16:08:22 +0900

    브로커에서 1초 주기로 메세지 polling 하도록 컨수머 구현

--- consumers/src/main/java/com/example/kafka/SimpleConsumer.java
+++ consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -2,13 +2,21 @@ package com.example.kafka;
 
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Properties;
 
 public class SimpleConsumer {
+
+    public static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class.getName());
+
     public static void main(String[] args) {
         String topic = "simple-topic";
 
@@ -25,6 +33,12 @@ public class SimpleConsumer {
         // consumer group 은 여러 개의 토픽 구독이 가능함.
         consumer.subscribe(List.of(topic));
 
-
+        while (true) {
+            // 1초 대기 후 batch 단위의 레코드들을 polling.
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord record: records) {
+                logger.info("record_key:{}, record_value:{}, partition:{}", record.key(), record.value(), record.partition());
+            }
+        }
     }
 }
