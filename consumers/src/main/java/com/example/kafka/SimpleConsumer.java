9fcd3b6c04ea9f02272b5cfd3d9c53c7f51593df
daeun.kim <daeun.kim@wesang.com>
2023-01-28 15:47:49 +0900

    카프카 컨수머 객체 생성

--- consumers/src/main/java/com/example/kafka/SimpleConsumer.java
+++ consumers/src/main/java/com/example/kafka/SimpleConsumer.java
@@ -1,4 +1,30 @@
 package com.example.kafka;
 
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.List;
+import java.util.Properties;
+
 public class SimpleConsumer {
+    public static void main(String[] args) {
+        String topic = "simple-topic";
+
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        // consumer group identifier 설정
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group_01");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        // consumer group 은 여러 개의 토픽 구독이 가능함.
+        consumer.subscribe(List.of(topic));
+
+
+    }
 }
