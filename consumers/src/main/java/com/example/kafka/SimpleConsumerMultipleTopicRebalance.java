5d1f9d8cc18b5f73331ede718481db6b56e02b3b
daeun.kim <daeun.kim@wesang.com>
2023-01-31 19:42:43 +0900

    2개 토픽에서 메세지를 받는 컨수머 생성

---
+++ consumers/src/main/java/com/example/kafka/SimpleConsumerMultipleTopicRebalance.java
@@ -0,0 +1,64 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+
+public class SimpleConsumerMultipleTopicRebalance {
+
+    public static final Logger logger = LoggerFactory.getLogger(SimpleConsumer.class.getName());
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // consumer group identifier 설정
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-multiple-topic");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+        // consumer group 은 여러 개의 토픽 구독이 가능함.
+        consumer.subscribe(List.of("topic-p3-t1", "topic-p3-t2"));
+
+        // 별도 쓰레드에서 client.wakeup() 호출 -> 메인쓰레드에서 WakeupException 발생
+        Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                logger.info("");
+                consumer.wakeup();
+                try { mainThread.join(); }
+                catch (InterruptedException e) { e.printStackTrace(); }
+            }
+        });
+
+        // shutdown kafka consumption gracefully...
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+                for (ConsumerRecord record: records) {
+                    logger.info("topic: {} record_key:{}, record_value:{}, partition:{}, record's offset:{}",
+                            record.topic(), record.key(), record.value(), record.partition(), record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            logger.error("wakeup exception has occurred.");
+        } finally {
+            logger.info("kafka consumer is closing...");
+            consumer.close();
+        }
+
+    }
+}
