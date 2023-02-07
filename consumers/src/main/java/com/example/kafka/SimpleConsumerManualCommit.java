0e69cbb9f4544ee8d2b5e263f1bbc84bc5a72924
daeun.kim <daeun.kim@wesang.com>
2023-02-07 18:32:04 +0900

    비동기 방식으로 직접 커밋하는 메서드 구현

--- consumers/src/main/java/com/example/kafka/SimpleConsumerManualCommit.java
+++ consumers/src/main/java/com/example/kafka/SimpleConsumerManualCommit.java
@@ -1,6 +1,7 @@
 package com.example.kafka;
 
 import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -8,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 
@@ -26,7 +28,7 @@ public class SimpleConsumerManualCommit {
         props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // consumer group identifier 설정
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-01");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-05");
 
         // manual commit 설정
         props.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
@@ -47,8 +49,46 @@ public class SimpleConsumerManualCommit {
             }
         });
 
-        pollCommitSync(consumer);
+//        pollCommitSync(consumer);
+        pollCommitAsync(consumer);
 
+    }
+
+    private static void pollCommitAsync(KafkaConsumer<String, String> consumer) {
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(10000));
+                for (ConsumerRecord record: records) {
+                    logger.info("record_key:{}, record_value:{}, partition:{}, record's offset:{}", record.key(), record.value(), record.partition(), record.offset());
+                }
+                try {
+                    // record 순회할 때마다 커밋 하는 게 아니라 batch 단위로 커밋.
+                    // 비동기 방식은 커밋 결과를 콜백으로 전달함.
+                    if(records.count() > 0) {
+                        consumer.commitAsync(new OffsetCommitCallback() {
+                            @Override
+                            public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+                                if (exception != null) {
+                                    logger.error("offset {} is not completed. error: {}", offsets, exception);
+                                }
+                                else {
+                                    logger.info("offset {} has been committed successfully.", offsets);
+                                }
+                            }
+                        });
+                        logger.info("commitAsync has been called.");
+                    }
+                } catch (CommitFailedException e) {
+                    logger.error(e.getMessage());
+                }
+
+            }
+        } catch (WakeupException e) {
+            logger.error("wakeup exception has occurred.");
+        } finally {
+            logger.info("kafka consumer is closing...");
+            consumer.close();
+        }
     }
 
     private static void pollCommitSync(KafkaConsumer<String, String> consumer) {
